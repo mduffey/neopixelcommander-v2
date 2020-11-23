3d0fc1e555e246e358d8ba5df0d5a2689a01d4ba
Michael Duffey <mduffey07@gmail.com>
2020-11-23 04:17:29 -0600

    Remove old versions of the Arduino code

--- Arduino/LEDTesting/Serial.ino
+++
@@ -1,8 +0,0 @@
-void PrintLEDUpdate(int red, int green, int blue) {
-    Serial.print(F(". Colors: "));
-    Serial.print(red);
-    Serial.print(", ");
-    Serial.print(green);
-    Serial.print(", ");
-    Serial.println(blue);
-}
